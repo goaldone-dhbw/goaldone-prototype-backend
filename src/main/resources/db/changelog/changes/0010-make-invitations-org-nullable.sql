3e702378405aa25a514f0d4305c8663644b7d075
Joinsider <johannes@fampopprol.me>
2026-03-21 17:02:32 +0100

    Make organization_id nullable in invitations table

--- src/main/resources/db/changelog/changes/0010-make-invitations-org-nullable.sql
+++ src/main/resources/db/changelog/changes/0010-make-invitations-org-nullable.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
 --changeset goaldone:0010-make-invitations-org-nullable
-ALTER TABLE invitations ALTER COLUMN organization_id UUID NULL;
---rollback ALTER TABLE invitations ALTER COLUMN organization_id UUID NOT NULL;
+ALTER TABLE invitations ALTER COLUMN organization_id DROP NOT NULL;
+--rollback ALTER TABLE invitations ALTER COLUMN organization_id SET NOT NULL;
