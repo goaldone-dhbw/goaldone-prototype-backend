557e80f97793888aadbac67c6d6e62a201d7a535
Joinsider <johannes@fampopprol.me>
2026-04-26 18:03:46 +0200

    Add environment variable configurations and update Docker setup for Traefik integration

--- src/main/java/de/goaldone/backend/service/AuthService.java
+++ src/main/java/de/goaldone/backend/service/AuthService.java
@@ -22,7 +22,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
